7dc24759fbf465fb480ad22140c9d18833d161ff
yaroslavfq <57676746+yrslvfq@users.noreply.github.com>
2026-03-11 12:21:23 +0300

    Add files via upload

---
+++ типы данных/hello.py
@@ -0,0 +1,21 @@
+a = 42
+b = 3.14159
+c = "Hello, Python!"
+d = True
+e = [1, 2, 3]
+f = (4, 5, 6)
+g = {"name": "Alice", "age": 30}
+h = {7, 8, 9}
+i = None
+print ("")
+
+
+print(f"a = {a}, тип: {type(a)}")
+print(f"b = {b}, тип: {type(b)}")
+print(f"c = {c}, тип: {type(c)}")
+print(f"d = {d}, тип: {type(d)}")
+print(f"e = {e}, тип: {type(e)}")
+print(f"f = {f}, тип: {type(f)}")
+print(f"g = {g}, тип: {type(g)}")
+print(f"h = {h}, тип: {type(h)}")
+print(f"i = {i}, тип: {type(i)}")
